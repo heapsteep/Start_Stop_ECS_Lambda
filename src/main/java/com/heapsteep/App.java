1f77d0305b7e2c4cbdef68ab94a1f8851f214c08
Prasanna Sahu <prasannasahu4u@gmail.com>
2020-12-02 14:47:31 +0530

    New changes

--- src/main/java/com/heapsteep/App.java
+++ src/main/java/com/heapsteep/App.java
@@ -20,6 +20,7 @@ import com.amazonaws.services.ecs.model.UpdateServiceRequest;
 import com.amazonaws.services.ecs.model.UpdateServiceResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.heapsteep.resources.Manager;
 
 public class App implements RequestHandler<Object,Object>{
 	private final static Logger LOGGER = Logger.getLogger(App.class.getName());
@@ -31,22 +32,20 @@ public class App implements RequestHandler<Object,Object>{
 		AmazonECS client=AmazonECSClientBuilder.standard().withRegion("ap-south-1").build();
 		
 		Yaml yaml=new Yaml();
-		String path=".\\src\\main\\java\\com\\heapsteep\\resources\\application.yaml";
-		List fullList=null;
-		try {
-			Object obj=yaml.load(new FileInputStream(new File(path)));
-			System.out.println(obj);
-			fullList=(ArrayList)obj;
-		}catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		InputStream is=this.getClass().getClassLoader().getResourceAsStream("application.yaml");		
 		
+		List fullList=null;
+		
+		Object obj=yaml.load(is);
+		LOGGER.info("application.yaml values: "+obj.toString());
+		fullList=(ArrayList)obj;
+		
+		String finalMessage=null;
 		for (Object obj2 : fullList) { 
 			Map map=(Map)obj2;
 			Iterator it = map.entrySet().iterator();
 			while (it.hasNext()) {
 				Map.Entry set = (Map.Entry) it.next();
-				//System.out.println(set.getKey() + " = " + set.getValue());
 				String clusterName=(String)set.getKey();
 				List servicesList=(List)set.getValue();
 				for (Object obj3 : servicesList) { 
@@ -54,25 +53,22 @@ public class App implements RequestHandler<Object,Object>{
 					Iterator it2 = map2.entrySet().iterator();
 					while (it2.hasNext()) {
 						Map.Entry set2 = (Map.Entry) it2.next();
-						//System.out.println(set2.getKey() + " => " + set2.getValue());
-						System.out.println("Cluster:" + clusterName + ",Service:"+set2.getKey()+",desiredCount:"+set2.getValue());
+						LOGGER.info("Cluster:" + clusterName + ",Service:"+set2.getKey()+",desiredCount:"+set2.getValue());
 						
 						if(resourceArr[1].equals("Stop_ECS")) {
 							UpdateServiceRequest request=new UpdateServiceRequest().withCluster(clusterName).withService(set2.getKey().toString()).withDesiredCount(0);
 							
 							UpdateServiceResult response=client.updateService(request);	       
 					        LOGGER.info("===>Message:" + response.getService().getEvents().get(3).getMessage());
-					        
-					        LOGGER.info("*******All ECS Services are Stopped as part of Weekend Shutdown********");
-							return "All ECS Services are Stopped as part of Weekend Shutdown";
+					        					        
+							finalMessage= "*******All ECS Services are Stopped as part of Weekend Shutdown********";
 						}else if(resourceArr[1].equals("Start_ECS")) {
 							UpdateServiceRequest request=new UpdateServiceRequest().withCluster(clusterName).withService(set2.getKey().toString()).withDesiredCount((Integer)set2.getValue());
 							
 							UpdateServiceResult response=client.updateService(request);			
 					        LOGGER.info("===>Message:" + response.getService().getEvents().get(3).getMessage());
-					        
-					        LOGGER.info("*******All ECS Services are Started as part of Weekday Startup*******");
-							return "All ECS Services are Started as part of Weekday Startup";
+					        					       
+					        finalMessage= "*******All ECS Services are Started as part of Weekday Startup*******";
 						}
 						
 					}
@@ -101,7 +97,8 @@ public class App implements RequestHandler<Object,Object>{
 	        
 	        LOGGER.info("*******All ECS Services are Started as part of Weekday Startup*******");
 			return "All ECS Services are Started as part of Weekday Startup";			
-		}*/		
-		return null;
+		}*/	
+		LOGGER.info(finalMessage);
+		return finalMessage;
 	}
 }
